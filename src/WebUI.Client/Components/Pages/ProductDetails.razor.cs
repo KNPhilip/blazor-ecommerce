6dc652119fd477d59a60fd5c8107e2641231a91e
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-09-23 22:31:09 +0200

    Add Publisher to Product Details page

--- src/WebUI.Client/Components/Pages/ProductDetails.razor.cs
+++ src/WebUI.Client/Components/Pages/ProductDetails.razor.cs
@@ -39,4 +39,33 @@ public sealed partial class ProductDetails
 
         await CartUIService.AddToCartAsync(cartItem);
     }
+
+    private string GetPublishers()
+    {
+        if (product?.Publishers is null || product.Publishers.Count == 0)
+            return "";
+
+        List<string> names = product.Publishers.Select(p =>
+        {
+            if (!string.IsNullOrWhiteSpace(p.NickName))
+                return p.NickName;
+            else
+                return $"{p.FirstName} {p.LastName}";
+        }).ToList();
+
+        if (names.Count == 1)
+        {
+            return names[0];
+        }
+        else if (names.Count == 2)
+        {
+            return $"{names[0]} and {names[1]}";
+        }
+        else
+        {
+            var allButLast = string.Join(", ", names.Take(names.Count - 1));
+            var last = names.Last();
+            return $"{allButLast}, and {last}";
+        }
+    }
 }
