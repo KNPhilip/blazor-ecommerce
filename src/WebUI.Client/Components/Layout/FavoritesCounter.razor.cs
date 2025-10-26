55e16636648d2fb3af051b834df46599def002d4
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-10-26 21:26:54 +0100

    Add Favorites to Layout

---
+++ src/WebUI.Client/Components/Layout/FavoritesCounter.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace WebUI.Client.Components.Layout;
+
+public sealed partial class FavoritesCounter
+{
+    protected override void OnInitialized()
+    {
+        FavoriteUIService.OnFavoritesChanged += StateHasChanged;
+        FavoriteUIService.GetFavoritesAsync();
+    }
+
+    public void Dispose()
+    {
+        FavoriteUIService.OnFavoritesChanged -= StateHasChanged;
+    }
+}
